1e197f69ff6ea2a230ee358e024447f9e5c98600
sokangmin <mini02041!>
2018-10-25 09:06:27 +0900

    Initial commit

---
+++ src/main/java/com/winitech/demoentitygraph/DemoentitygraphApplication.java
@@ -0,0 +1,12 @@
+package com.winitech.demoentitygraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoentitygraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoentitygraphApplication.class, args);
+	}
+}
---
+++ src/test/java/com/winitech/demoentitygraph/DemoentitygraphApplicationTests.java
@@ -0,0 +1,16 @@
+package com.winitech.demoentitygraph;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoentitygraphApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
